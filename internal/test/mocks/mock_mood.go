ea990b9c7a457071643903e75d075da513489a8b
Rishab <rishab28guleria@gmail.com>
2026-01-11 13:41:10 +0530

    Added Mocks as per current implementation. (Still requires logical changes)

--- internal/test/mocks/mock_mood.go
+++ internal/test/mocks/mock_mood.go
@@ -2,6 +2,7 @@ package mocks
 
 import (
 	"context"
+	"time"
 
 	"github.com/RGisanEclipse/NeuroNote-Server/internal/models/mood"
 	"github.com/stretchr/testify/mock"
@@ -12,5 +13,13 @@ type MockMoodRepo struct{ mock.Mock }
 func (m *MockMoodRepo) SaveMood(ctx context.Context, data mood.Entry) error {
 	args := m.Called(ctx, data)
 	return args.Error(0)
+}
+
+func (m *MockMoodRepo) GetMoodByDuration(ctx context.Context, userId string, from time.Time) ([]mood.Entry, error) {
+	args := m.Called(ctx, userId, from)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).([]mood.Entry), args.Error(1)
 }
 
--- internal/test/mocks/mock_mood_service.go
+++ internal/test/mocks/mock_mood_service.go
@@ -15,3 +15,11 @@ func (m *MockMoodService) LogMood(ctx context.Context, userId string, request mo
 	return args.Bool(0), args.Get(1).(*appError.Code)
 }
 
+func (m *MockMoodService) GetMood(ctx context.Context, userId string, days int) ([]mood.Entry, *appError.Code) {
+	args := m.Called(ctx, userId, days)
+	if args.Get(0) == nil {
+		return nil, args.Get(1).(*appError.Code)
+	}
+	return args.Get(0).([]mood.Entry), args.Get(1).(*appError.Code)
+}
+
